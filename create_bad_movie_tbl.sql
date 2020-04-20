49ddaa83bdf1e09e0f6d8b645bea2cc718c094b8
IAmVeryConnorSimmons <45773562+IAmVeryConnorSimmons@users.noreply.github.com>
2020-04-20 13:56:36 -0400

    Initial bad movie table schema

---
+++ create_bad_movie_tbl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bad_movies (
+    movie_title VARCHAR (250) NOT NULL,
+    year INTEGER NULL,
+    fun INTEGER NULL,
+    inadvertently_funny INTEGER NULL,
+    bad_acting INTEGER NULL,
+    rewatchable INTEGER NULL,
+    accessibility INTEGER NULL,
+    production_quality INTEGER NULL,
+    family_friendly INTEGER NULL
+);
