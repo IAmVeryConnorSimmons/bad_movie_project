f0fca4f7361a30582f9875b93326e408d776017e
IAmVeryConnorSimmons <45773562+IAmVeryConnorSimmons@users.noreply.github.com>
2020-04-23 19:27:32 -0400

    Added DROP TABLE for replayability

--- create_bad_movie_tbl.sql
+++ create_bad_movie_tbl.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS bad_movies;
 CREATE TABLE bad_movies (
     movie_title VARCHAR (255) NOT NULL,
     release_year INTEGER NULL,
@@ -9,5 +10,5 @@ CREATE TABLE bad_movies (
     accessibility INTEGER NULL,
     production_quality INTEGER NULL,
     family_friendly INTEGER NULL,
-    most_memorable_performer VACHAR (255)
+    most_memorable_performer VARCHAR (255)
 );
