664374292637e04a3fae0df3e0cc70105bf536a2
IAmVeryConnorSimmons <45773562+IAmVeryConnorSimmons@users.noreply.github.com>
2020-04-23 19:20:46 -0400

    Updated column name

--- create_bad_movie_tbl.sql
+++ create_bad_movie_tbl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE bad_movies (
     movie_title VARCHAR (250) NOT NULL,
     year INTEGER NULL,
-    fun INTEGER NULL,
+    watchable INTEGER NULL,
     inadvertently_funny INTEGER NULL,
     bad_acting INTEGER NULL,
     rewatchable INTEGER NULL,
